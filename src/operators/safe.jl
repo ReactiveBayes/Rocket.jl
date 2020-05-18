5d3712cd27ec43af2eea2403ebbbd4cd7006e242
Dmitri Bagaev <bvdmitri@gmail.com>
2020-05-18 11:37:12 +0200

    feat(operators): rerun and safe operators fixes

--- src/operators/safe.jl
+++ src/operators/safe.jl
@@ -25,55 +25,61 @@ operator_right(operator::SafeOperator, ::Type{L}) where L = L
 
 struct SafeProxy <: ActorSourceProxy end
 
-actor_proxy!(::Type{L}, proxy::SafeProxy, actor::A)   where { L, A } = SafeActor{L, A}(actor, false, nothing)
+actor_proxy!(::Type{L}, proxy::SafeProxy, actor::A)   where { L, A } = SafeActor{L, A}(actor, SafeActorProps(false, voidTeardown))
 source_proxy!(::Type{L}, proxy::SafeProxy, source::S) where { L, S } = SafeSource{L, S}(source)
 
-mutable struct SafeActor{L, A} <: Actor{L}
-    actor :: A
-
-    is_failed            :: Bool
-    current_subscription :: Union{Nothing, Teardown}
+mutable struct SafeActorProps
+    isfailed     :: Bool
+    subscription :: Teardown
 end
 
+struct SafeActor{L, A} <: Actor{L}
+    actor :: A
+    props :: SafeActorProps
+end
+
+isfailed(actor::SafeActor)   = actor.props.isfailed
+setfailed!(actor::SafeActor) = actor.props.isfailed = true
+
+getsubscription(actor::SafeActor)         = actor.props.subscription
+setsubscription!(actor::SafeActor, value) = actor.props.subscription = value
+
 function on_next!(actor::SafeActor{L}, data::L) where L
-    if !actor.is_failed
+    if !isfailed(actor)
         try
             next!(actor.actor, data)
         catch exception
+            dispose!(actor)
             error!(actor.actor, exception)
-            __safe_actor_dispose(actor)
         end
     end
 end
 
 function on_error!(actor::SafeActor, err)
-    if !actor.is_failed
+    if !isfailed(actor)
         try
             error!(actor.actor, err)
         catch exception
+            dispose!(actor)
             error!(actor.actor, exception)
-            __safe_actor_dispose(actor)
         end
     end
 end
 
 function on_complete!(actor::SafeActor)
-    if !actor.is_failed
+    if !isfailed(actor)
         try
             complete!(actor.actor)
         catch exception
+            dispose!(actor)
             error!(actor.actor, exception)
-            __safe_actor_dispose(actor)
         end
     end
 end
 
-function __safe_actor_dispose(actor::SafeActor)
-    actor.is_failed = true
-    if actor.current_subscription !== nothing
-        unsubscribe!(actor.current_subscription)
-        actor.current_subscription = nothing
-    end
+function dispose!(actor::SafeActor)
+    setfailed!(actor)
+    unsubscribe!(getsubscription(actor))
 end
 
 struct SafeSource{L, S} <: Subscribable{L}
@@ -83,10 +89,11 @@ end
 function on_subscribe!(source::SafeSource, actor::SafeActor)
     try
         subscription = subscribe!(source.source, actor)
-        actor.current_subscription = subscription
+        setsubscription!(actor, subscription)
         return subscription
     catch exception
-        error!(actor, exception)
+        dispose!(actor)
+        error!(actor.actor, exception)
         return voidTeardown
     end
 end
